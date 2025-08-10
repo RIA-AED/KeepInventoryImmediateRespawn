592026e0aa483901fdff5ae60bd089cc5658b584
岩浆块Magma <magmablock@qq.com>
2025-08-10 21:32:18 +0800

    create

---
+++ src/main/java/ink/magma/keepInventoryImmediateRespawn/KeepInventoryImmediateRespawn.java
@@ -0,0 +1,17 @@
+package ink.magma.keepInventoryImmediateRespawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KeepInventoryImmediateRespawn extends JavaPlugin, List {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
