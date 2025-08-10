464644200777c3c662353d9cffb828328524ee91
岩浆块Magma <magmablock@qq.com>
2025-08-10 21:33:41 +0800

    feat: 实现保持物品栏即时重生功能
    - 添加玩家死亡事件监听器
    - 检查是否保持物品栏和即时重生游戏规则
    - 如果满足条件，在1tick后强制重生玩家
    
    build: 更新构建配置
    
    - 移除-SNAPSHOT版本后缀
    - 添加阿里云Maven仓库

--- src/main/java/ink/magma/keepInventoryImmediateRespawn/KeepInventoryImmediateRespawn.java
+++ src/main/java/ink/magma/keepInventoryImmediateRespawn/KeepInventoryImmediateRespawn.java
@@ -1,17 +1,37 @@
 package ink.magma.keepInventoryImmediateRespawn;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class KeepInventoryImmediateRespawn extends JavaPlugin, List {
+public final class KeepInventoryImmediateRespawn extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        boolean isKeepInv = event.getKeepInventory();
+        if (!isKeepInv) return;
+
+        boolean isWorldImmediateRespawn = Boolean.TRUE.equals(event.getEntity().getWorld().getGameRuleValue(GameRule.DO_IMMEDIATE_RESPAWN));
+        if (isWorldImmediateRespawn) return;
+
+        // 1 tick
+        Bukkit.getScheduler().runTask(this, () -> {
+            event.getEntity().spigot().respawn();
+        });
     }
 }
